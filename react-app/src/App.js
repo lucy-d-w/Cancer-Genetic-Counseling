56dc1e3091d0b2bda647b92e57eadf2c42ecba1f
emilymarkert <emily.markert@vanderbilt.edu>
2019-02-09 14:09:39 -0600

    making generic backbone component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Backbone from './Backbone'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Backbone title="Page 1"></Backbone>
+          <Backbone title="Page 2"></Backbone>
         </header>
       </div>
     );
---
+++ react-app/src/Backbone.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Backbone.css';
+
+const Backbone = ({title}) => (
+    <div className="BB-body">
+        <header className="BB-header">{title}</header>
+    </div>
+);
+
+export default Backbone;
