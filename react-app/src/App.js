c3824c14774df5d4dd3c381ef2c9533a9179dd60
emilymarkert <emily.markert@vanderbilt.edu>
2019-02-09 15:13:30 -0600

    adding backbones 3-9

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import Backbone from './Backbone'
 import Backbone2 from './Backbones/Backbone2'
+import Backbone3 from './Backbones/Backbone3'
+import Backbone4 from './Backbones/Backbone4'
 
 class App extends Component {
   render() {
@@ -11,6 +13,8 @@ class App extends Component {
         <header className="App-header">
           <Backbone title="Title Page"></Backbone>
           <Backbone2></Backbone2>
+          <Backbone3></Backbone3>
+          <Backbone4></Backbone4>
         </header>
       </div>
     );
---
+++ react-app/src/Backbones/Backbone4.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Backbone from '../Backbone';
+
+const Backbone4 = () => (
+    <Backbone title="Cancer and Inherited Genes">
+        <p>test</p>
+    </Backbone>
+)
+
+export default Backbone4;
