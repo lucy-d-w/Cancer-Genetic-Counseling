55096d555871deaa6de01d72027d3a8ce4827470
emilymarkert <emily.markert@vanderbilt.edu>
2019-02-10 19:24:32 -0600

    adding more graphics

--- react-app/src/Backbones/Backbone5.js
+++ react-app/src/Backbones/Backbone5.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Backbone from '../Backbone';
+import chart from '../Graphics/bb5-chart.png'
 
 class Backbone5 extends React.Component {
     render() {
         return (
             <Backbone title="Chance to Inherit a Cancer Risk Gene">
                 <div className="BB-content">
-                    
+                    <div className="Main-content" style={{textalign: 'center'}}>
+                        <img src={chart} className='Main-graphic' onClick={this.toggleOverlay}></img>
+                    </div>
                 </div>
             </Backbone>
         );
