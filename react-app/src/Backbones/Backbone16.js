997a440726bf80d3e0e76fbdfaf72fa986a7346d
lucy-d-w <lucy.d.wang@vanderbilt.edu>
2019-02-16 21:23:54 -0600

    15, 16

--- react-app/src/Backbones/Backbone16.js
+++ react-app/src/Backbones/Backbone16.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Backbone from '../Backbone';
 
-const Backbone16 = () => (
-    <Backbone title="What to Expect">
-        <p>test</p>
-    </Backbone>
-)
+class Backbone16 extends React.Component {
+    render() {
+        return (
+            <Backbone title="What to Expect">
+                <div className="BB-content">
+                   
+                    <div className={"Main-content Content-box"}>
+                        <li>Speak with healthcare provider</li>
+                        <li>Review and sign forms</li>
+                        <li>Provide a blood/saliva sample for testing</li>
+                        <li>Test results usually available within a few weeks</li>
+                        <li>These results can help personalize your care</li>
+                    </div>
+                   
+                </div>    
+            </Backbone>
+        );
+    }
+}
 
 export default Backbone16;
