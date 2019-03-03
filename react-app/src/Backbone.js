f660dd1e5ef53734bec68928e8d752bcb336de2b
Emily Markert <emily.markert@vanderbilt.edu>
2019-03-02 20:30:11 -0600

    adding formatting for scroll arrows

--- react-app/src/Backbone.js
+++ react-app/src/Backbone.js
@@ -1,14 +1,19 @@
 import React from "react";
 import './Backbone.css';
-import { Card, H2 } from "@blueprintjs/core";
+import { Card, H2, Icon } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
 
 
 
 const Backbone = (props) => (
-    <Card className="BB-body">
-        <H2 className="BB-header">{props.title}</H2>
-        <div className="Content">{props.children}</div>
-    </Card>
+    <div>
+        <Card className="BB-body">
+            <H2 className="BB-header">{props.title}</H2>
+            <div className="Content">{props.children}</div>
+        </Card>
+            <Icon className="Arrow" icon={IconNames.CHEVRON_DOWN} iconSize={Icon.SIZE_LARGE} ></Icon>
+       </div>
+    
 );
 
 export default Backbone;
