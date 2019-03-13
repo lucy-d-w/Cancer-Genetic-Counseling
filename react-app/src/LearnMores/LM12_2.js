4b57b8bccecd937d5dae46d970f7e4e21a240eb1
lucy-d-w <lucy.d.wang@vanderbilt.edu>
2019-03-12 20:08:33 -0500

    learn more 12.2

---
+++ react-app/src/LearnMores/LM12_2.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Backbone from '../Backbone';
+
+class LM_12_2 extends React.Component {
+    render() {
+        return (
+            <Backbone title="Negative Results" first={this.props.first} last={this.props.last}>
+                <div className="Content">
+                   
+                    <div className={"Main-content Content-box"}>
+                        <li>You may not have a cancer risk gene. If there is a cancer diagnosis, it may be sporadic (not inherited)</li>
+                        <li>Current testing identifies most but not all mutations, so a gene mutation may have been present in the tested gene but undetected</li>
+                        <li>A mutation may be present in a gene that was not tested</li>
+                    </div>
+                   
+                </div>    
+            </Backbone>
+        );
+    }
+}
+
+export default LM_12_2;
