7d7c234124c569caabb6702b20175ff2a5360a82
lucy-d-w <lucy.d.wang@vanderbilt.edu>
2019-03-12 20:12:13 -0500

    learn more 16.1

---
+++ react-app/src/LearnMores/LM16_1.js
@@ -0,0 +1,19 @@
+import React from "react";
+import LearnMore from '../LearnMore';
+import { H2, H4 } from "@blueprintjs/core";
+import counselingprocess from '../Graphics/counselingprocess.png'
+
+class LM_16_1 extends React.Component {
+    render() {
+        return (
+            <LearnMore title="Who should be tested first?" first={true} last={true}>
+                <div className="Content">
+                <div className="Main-content" style={{textalign: 'center'}}>
+                        <img src={counselingprocess} className='Main-graphic'></img>
+                    </div>
+                </div></LearnMore>
+        );
+    }
+}
+
+export default LM_16_1;
