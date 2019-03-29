069f13df2e021e8f74e5c41dd0d7902246f76bcc
Emily Markert <emily.markert@vanderbilt.edu>
2019-03-29 01:17:20 -0500

    learn mores for bb 5

--- react-app/src/LearnMores/LM5-3.js
+++ react-app/src/LearnMores/LM5-3.js
@@ -1,13 +1,17 @@
 import React from "react";
 import LearnMore from '../LearnMore';
-import { H2 } from "@blueprintjs/core";
+import chart from '../Graphics/lm5-3-chart.png'
 
 class LM_5_3 extends LearnMore {
     render() {
         return (
             <LearnMore title="How Genes Cause Cancer" first={false} last={false}> {/*FIX!!!!*/}
                 <div className="BB-content">
-
+                    <div className="Main-content" style={{ textalign: 'center' }}>
+                        <div className="Main-graphic-wrap">
+                            <img src={chart} className='Main-graphic'></img>
+                        </div>
+                    </div>
                 </div>
             </LearnMore>
         );
