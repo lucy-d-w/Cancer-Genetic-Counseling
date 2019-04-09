2a299b8415ea790ca4cf9da07d31397a2729e4cf
lucy-d-w <lucy.d.wang@vanderbilt.edu>
2019-04-09 10:28:01 -0500

    ugly change that hopefully client will take back

--- react-app/src/Backbones/Backbone17.js
+++ react-app/src/Backbones/Backbone17.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Backbone from '../Backbone';
-import end from '../Graphics/end.png'
+import end2 from '../Graphics/end2.png'
 
 const Backbone17 = () => (
     <Backbone title=" " last={true}>
-        <img src={end} className='main-graphic'  style={{width: 300}}></img>
+        <img src={end2} className='main-graphic'  style={{marginTop: '-10vh'}}></img>
     </Backbone>
 )
 
