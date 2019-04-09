cf9165d424200430adc4590136ba9e9e94cff45a
lucy-d-w <lucy.d.wang@vanderbilt.edu>
2019-04-09 16:10:41 -0500

    backbone and track changes

---
+++ react-app/src/Backbones/Backbone11_3.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Backbone from '../Backbone';
+import implications2 from '../Graphics/implications2.png'
+
+class Backbone11_3 extends React.Component {
+    render() {
+        return (
+
+            <Backbone title="What does a positive genetic test result mean?" first={this.props.first} last={this.props.last}>
+                <div className="Content">
+                    <div className="Main-content" style={{textalign: 'center'}}>
+                        <img src={implications2} className='Main-graphic'></img>
+                    </div>
+                </div>
+            </Backbone>
+        );
+    }
+}
+
+export default Backbone11_3;
