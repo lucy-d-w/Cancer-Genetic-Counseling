d7b2ec609eaa79ae7764713fbf0896ec86a4079c
lucy-d-w <lucy.d.wang@vanderbilt.edu>
2019-04-09 10:52:09 -0500

    another silly client edit

--- react-app/src/Backbones/Backbone5.js
+++ react-app/src/Backbones/Backbone5.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Backbone from '../Backbone';
-import chart from '../Graphics/bb5-chart.png'
+import chart2 from '../Graphics/chart2.png'
 import LM_5_1 from '../LearnMores/LM5-1'
 import LM_5_2 from '../LearnMores/LM5-2'
 import LM_5_3 from '../LearnMores/LM5-3'
@@ -15,13 +15,13 @@ class Backbone5 extends Backbone {
                     <div className="BB-content" style={{ height: '45vh' }}>
                         <div className="Main-content" style={{ textalign: 'center' }}>
                             <div className="Main-graphic-wrap">
-                                <img src={chart} className='Main-graphic'></img>
+                                <img src={chart2} className='Main-graphic' style={{marginTop: '-3vh'}}></img>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="Row">
-                    <div className="BB-content" style={{ height: '5vh' }}>
+                    <div className="BB-content" style={{ height: '5vh', marginTop: '10vh' }}>
                         <Overlay text="Learn More: Cancer Risk Genes" over={LM_5_1} />
                         <Overlay text="Learn More: Types of Testing" over={LM_5_2} />
                     </div>
