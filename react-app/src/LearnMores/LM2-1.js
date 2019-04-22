e422c2ff7ef01118e4ee441b7d61626bf0a23b72
lucy-d-w <lucy.d.wang@vanderbilt.edu>
2019-04-22 00:44:31 -0500

    remaining graphics. can't see, so please help me format

--- react-app/src/LearnMores/LM2-1.js
+++ react-app/src/LearnMores/LM2-1.js
@@ -1,6 +1,9 @@
 import React from "react";
 import LearnMore from '../LearnMore';
 import { H2, H4 } from "@blueprintjs/core";
+import environment from "../Graphics/environment.png";
+import both from "../Graphics/both.png";
+import genetic from "../Graphics/genetic.png";
 
 class LM_2_1 extends LearnMore {
     render() {
@@ -11,16 +14,19 @@ class LM_2_1 extends LearnMore {
                         <H2>Sporadic</H2>
                         <li>Most cancers</li>
                         <li>General population risk to develop cancer</li>
+                        <img src={environment} className='Graphic'></img>
                     </div>
                     <div className="Column Content-box">
                         <H2>Hereditary</H2>
                         <li>Shared genetic and environmental risk factors</li>
                         <li>Cancer risk is higher than general population risk </li>
+                        <img src={both} className='Graphic'></img>
                     </div>
                     <div className="Column Content-box">
                         <H2>Familial</H2>
                         <li>Single genetic risk factor</li>
                         <li>Cancer risk is very high regardless of lifestyle or environment</li>
+                        <img src={genetic} className='Graphic'></img>
                     </div>
 
                 </div>
--- react-app/src/LearnMores/LM5-1.js
+++ react-app/src/LearnMores/LM5-1.js
@@ -1,13 +1,16 @@
 import React from "react";
 import LearnMore from '../LearnMore';
 import { H2 } from "@blueprintjs/core";
+import genes from "../Graphics/genes.png";
 
 class LM_5_1 extends LearnMore {
     render() {
         return (
             <LearnMore title="How Genes Cause Cancer" first={true} last={true}>
-                <div className="BB-content">
-                    
+                <div className="Content">
+                    <div className="Main-content">
+                        <img src={genes} className='Main-graphic'></img>
+                    </div>
                 </div>
             </LearnMore>
         );
@@ -15,3 +18,4 @@ class LM_5_1 extends LearnMore {
 }
 
 export default LM_5_1;
+
